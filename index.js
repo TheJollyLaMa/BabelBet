402821b9f10d50d06f50305b1c2c62bb9f877ecd
TheJollyLaMa <j@caffeinelamanna.com>
2021-11-27 09:07:00 -0500

    added getChallenges() function to ChallengeToken. working on watcher logic

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
 var createError = require('http-errors');
-// import { createError } from './http-errors.js';
 var express = require('express');
 var path = require('path');
 var cron = require('node-cron');
 require('dotenv').config();
-// const Duolingo = require('duolingo-api-js');
-// cannot 'require' duolingo api. needed to be imported as per ES6 standard ... as in it does not jive well with the othe require statements.
-// '* */12 * * *' every 12 hours - check the challenge token list for broken and completed streaks
-cron.schedule('*/60 * * * * *', async () => {
-  const Web3 = require('web3');
-  const CT_json = require('./abis/ChallengeToken.json');
-  const CT_X_json = require('./abis/CT_X.json');
+const Duolingo = require('duolingo-api-js'); // could not 'require' duolingo api. It is an ES module and needed to be imported as per ES6 standard. ** Changed the api to run CommonJs
 
-  let web3 = new Web3('ws://localhost:7545');
-  const blockNum = await web3.eth.getBlockNumber(function (error, result) {console.log("Block Number at time of scheduled Cron Job: ", result)});
-  const account = await web3.eth.getAccounts().then((res) => {console.log("Web3 Account 1:\n", res[0]);return res[0];})
-  const CT_Contract = await web3.eth.net.getId().then(function(net_id){
-    console.log("Current Net Id:", net_id);
-     if(CT_json.networks[net_id]) {const CT_ContractAddress = CT_json.networks[net_id].address;var c = new web3.eth.Contract(CT_json.abi, CT_ContractAddress);
-      return c;}//else{return $window.alert("Challenge Token Smart contract not connected to selected network.")}
-  });
-
-  const challenges = await CT_Contract.methods.getChallenges().call().then((res)=>{return res});
-  console.log(challenges);
-  console.log('running a task every sixty seconds');
-  // for(key in object) {
-
-  // }
-});
 
 var indexRouter = require('./routes/index');
 var publicRouter = require('./routes/public');
@@ -62,6 +39,65 @@ app.use('/ViewChallenge', viewChallengeRouter);
 app.use('/public/index.html', express.static('public/index.html'));
 app.use('/public', publicRouter);
 
+// '* */12 * * *' every 12 hours - check the challenge token list for broken and completed streaks
+cron.schedule('*/30 * * * * *', async () => {
+  const Web3 = require('web3');
+  const CT_json = require('./abis/ChallengeToken.json');
+  const CT_X_json = require('./abis/CT_X.json');
+
+  let web3 = new Web3('ws://localhost:7545');
+  const blockNum = await web3.eth.getBlockNumber(function (error, result) {console.log("Block Number at time of scheduled Cron Job: ", result)});
+  const account = await web3.eth.getAccounts().then((res) => {console.log("Web3 Account 1:\n", res[0]);return res[0];})
+  const CT_Contract = await web3.eth.net.getId().then(function(net_id){
+    console.log("Current Net Id:", net_id);
+     if(CT_json.networks[net_id]) {
+       const CT_ContractAddress = CT_json.networks[net_id].address;
+       var c = new web3.eth.Contract(CT_json.abi, CT_ContractAddress);
+      return c;}//else{return $window.alert("Challenge Token Smart contract not connected to selected network.")}
+  });
+  /* get challenges */
+  // returning empty array for some unknown reason ....
+  const challenges = await CT_Contract.methods.getChallenges().call().then((res)=>{return res;});
+  console.log(challenges);
+
+  /*  For Each Challenge ...   */
+  for(var i=0; i <= challenges.length; i++){
+      /* --  Decode mint data to get both players names and streak_days. have to encode passwords for duolingo i think ... -- */
+      /* if mint data says challenge is paid, ignore it.
+          otherwise
+       Get Duo info of Both/All Players on the challenge's PlayerList */
+
+      /*-- for each player on the playerlist for this challenge --*/
+      player = {};
+      for(var j=0; j <= challenges[i].playerlist.length; j++){
+        var _name = challenges[i].playerlist[j][2];  //<-- looking for the third item in that player's spot in the playerlist - which should be that player's Duolingo name
+        var player[j] = Duolingo.Duolingo(_name, _pass);
+        var player[j]_duo_info = player[j].get_user_info();
+        /* If streak has not ended and goal has not been met, nothing happens - duolingo students continue showing up for language training every day!  */
+        /* if ( players_current_streak <= this_challenge_streak_days && player_streak =! 0) {
+                // Play on Garth
+                // Play On Wayne
+         }else{
+         If goal has been met - meaning a player or players have a streak that is greater than the challenge streak goal,
+             pay player their stake back plus their divided portion of forfeitures and interest earned on escrow sweep
+             total pot is split amongst the players that completed the challenge successfully
+              if ( players_current_streak > this_challenge_streak_days ) {
+                //
+              }
+        }
+        }
+
+
+
+
+          challenge is somehow marked as paid
+      */
+
+      /* emails to all players informing of payout and challenge completion with link to challenge page where players can view stats and past challeneges*/
+  }
+  console.log('running a task every sixty seconds');
+
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
